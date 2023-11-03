91a0e4318c2fd54fa27a3717ba485caa22ba7b3d
mylove90 <engm.saad1990@gmail.com>
2023-11-03 11:44:58 +0200

    Introduce PureCAF Kernel remove unused functions

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Ryzen Kernel by Edwiin Kusuma Jaya @ telegram
+kernel.string=PureCAF by mylove90 @ XDA-Devs
 do.devicecheck=1
 do.modules=1
 do.systemless=0
@@ -40,50 +40,6 @@ patch_vbmeta_flag=auto;
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
 # end ramdisk changes
-
-if mountpoint -q /data; then
-  # Optimize F2FS extension list (@arter97)
-  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
-    hash="$(md5sum $list_path | sed 's/extenstion/extension/g' | cut -d' ' -f1)"
-
-    # Skip update if our list is already active
-    if [[ $hash == "43df40d20dcb96aa7e8af0e3d557d086" ]]; then
-      echo "Extension list up-to-date: $list_path"
-      continue
-    fi
-
-    ui_print "  • Optimizing F2FS extension list"
-    echo "Updating extension list: $list_path"
-
-    echo "Clearing extension list"
-
-    hot_count="$(grep -n 'hot file extens' $list_path | cut -d':' -f1)"
-    list_len="$(cat $list_path | wc -l)"
-    cold_count="$((list_len - hot_count))"
-
-    cold_list="$(head -n$((hot_count - 1)) $list_path | grep -v ':')"
-    hot_list="$(tail -n$cold_count $list_path)"
-
-    for ext in $cold_list; do
-      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
-    done
-
-    for ext in $hot_list; do
-      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
-    done
-
-    echo "Writing new extension list"
-
-    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
-      [ ! -z $ext ] && echo "[c]$ext" > $list_path
-    done
-
-    for ext in $(cat $home/f2fs-hot.list); do
-      [ ! -z $ext ] && echo "[h]$ext" > $list_path
-    done
-  done
-fi
-
 
 flash_boot;
 flash_dtbo;
