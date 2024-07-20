fcdda2f1229c7e08e570d2282b2ce900ce367546
mylove90 <engm.saad1990@gmail.com>
2024-07-20 11:00:42 +0000

    Introduce PureMTK kernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,18 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
-do.modules=0
-do.systemless=1
+kernel.string=PureMtk by MY♡90 @ xda-developers
+do.devicecheck=0
+do.modules=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=
+device.name2=
+device.name3=
+device.name4=
 device.name5=
-supported.versions=
+supported.versions=14
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
